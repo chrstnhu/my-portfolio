0d515526a32bf089dc6296d897baeb5f7476e660
chrstnhu <chrhu@student.42.fr>
2026-09-22 23:33:25 +0200

    update project page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,109 +1,209 @@
 export const projectsData = [
   {
     title: "libft",
+    category: "c",
     descriptionKey: "projectDescriptions.libft",
     img: "/assets/projects/libft.jpg",
     link: "https://github.com/chrstnhu/libft_library",
+    technologies: ["C", "Makefile"],
+    skills: ["skillsUsed.memoryManagement", "skillsUsed.algorithms"],
   },
+
   {
     title: "get-next-line",
+    category: "c",
     descriptionKey: "projectDescriptions.getNextLine",
     img: "/assets/projects/get-next-line.png",
     link: "https://github.com/chrstnhu/get-next-line",
+    technologies: ["C", "File descriptors"],
+    skills: ["skillsUsed.fileHandling", "skillsUsed.memoryManagement"],
   },
+
   {
     title: "ft_printf",
+    category: "c",
     descriptionKey: "projectDescriptions.ftPrintf",
     img: "/assets/projects/ft_printf.png",
     link: "https://github.com/chrstnhu/ft_printf",
+    technologies: ["C", "Variadic functions"],
+    skills: ["skillsUsed.parsing", "skillsUsed.variadicFunctions"],
   },
+
   {
     title: "Born2beroot",
+    category: "system",
     descriptionKey: "projectDescriptions.born2beroot",
     img: "/assets/projects/VirtualBox.png",
+    technologies: ["Linux", "VirtualBox", "SSH"],
+    skills: ["skillsUsed.systemAdministration", "skillsUsed.security"],
   },
+
   {
     title: "push_swap",
+    category: "c",
     descriptionKey: "projectDescriptions.pushSwap",
     img: "/assets/projects/push_swap.png",
     link: "https://github.com/chrstnhu/push_swap",
+    technologies: ["C", "Algorithms"],
+    skills: [
+      "skillsUsed.algorithms",
+      "skillsUsed.complexity",
+      "skillsUsed.dataStructures",
+    ],
   },
+
   {
     title: "so_long",
+    category: "graphics",
     descriptionKey: "projectDescriptions.soLong",
     img: "/assets/projects/so_long.png",
     link: "https://github.com/chrstnhu/so_long",
+    technologies: ["C", "MiniLibX"],
+    skills: ["skillsUsed.graphics", "skillsUsed.eventHandling"],
   },
+
   {
     title: "pipex",
+    category: "system",
     descriptionKey: "projectDescriptions.pipex",
     img: "/assets/projects/Pipex.png",
     link: "https://github.com/chrstnhu/pipex",
+    technologies: ["C", "Unix"],
+    skills: [
+      "skillsUsed.processes",
+      "skillsUsed.pipes",
+      "skillsUsed.fileDescriptors",
+    ],
   },
+
   {
     title: "philosophers",
+    category: "c",
     descriptionKey: "projectDescriptions.philosophers",
     img: "/assets/projects/philosophers.png",
     link: "https://github.com/chrstnhu/philosophers",
+    technologies: ["C", "Threads", "Mutex"],
+    skills: ["skillsUsed.multithreading", "skillsUsed.synchronization"],
   },
+
   {
     title: "minishell",
+    category: "system",
     descriptionKey: "projectDescriptions.minishell",
     img: "/assets/projects/minishell.png",
     link: "https://github.com/chrstnhu/minishell",
+    technologies: ["C", "Unix", "Bash"],
+    skills: ["skillsUsed.shell", "skillsUsed.parsing", "skillsUsed.processes"],
   },
+
   {
     title: "NetPractice",
+    category: "networking",
     descriptionKey: "projectDescriptions.netPractice",
     img: "/assets/projects/NetPractice.png",
     link: "https://github.com/chrstnhu/net-practice",
+    technologies: ["TCP/IP", "Networking"],
+    skills: ["skillsUsed.networking", "skillsUsed.ipAddressing"],
   },
+
   {
     title: "cub3d",
+    category: "graphics",
     descriptionKey: "projectDescriptions.cub3d",
     img: "/assets/projects/cub3D.png",
     link: "https://github.com/chrstnhu/cub3d",
+    technologies: ["C", "MiniLibX", "Raycasting"],
+    skills: [
+      "skillsUsed.raycasting",
+      "skillsUsed.graphics",
+      "skillsUsed.algorithms",
+    ],
   },
+
   {
     title: "CPP Modules",
+    category: "cpp",
     descriptionKey: "projectDescriptions.cppModules",
     img: "/assets/projects/C++.png",
     link: "https://github.com/chrstnhu/CPP-Module",
+    technologies: ["C++", "Object-Oriented Programming"],
+    skills: [
+      "skillsUsed.oop",
+      "skillsUsed.inheritance",
+      "skillsUsed.polymorphism",
+    ],
   },
+
   {
     title: "inception",
+    category: "system",
     descriptionKey: "projectDescriptions.inception",
     img: "/assets/projects/Docker.png",
     link: "https://github.com/chrstnhu/inception",
+    technologies: ["Docker", "Linux", "Nginx", "MariaDB"],
+    skills: ["skillsUsed.containerization", "skillsUsed.systemAdministration"],
   },
+
   {
     title: "ft_irc",
+    category: "networking",
     descriptionKey: "projectDescriptions.ftIrc",
     img: "/assets/projects/ft_irc.png",
     link: "https://github.com/chrstnhu/ft_irc",
+    technologies: ["C++", "Sockets", "IRC"],
+    skills: ["skillsUsed.networkProgramming", "skillsUsed.sockets"],
   },
+
   {
     title: "ft_transcendence",
+    category: "web",
     descriptionKey: "projectDescriptions.ftTranscendence",
     img: "/assets/projects/ft_transcendence.png",
     link: "https://github.com/chrstnhu/ft_transcendence",
+    technologies: ["React", "TypeScript", "Node.js", "Express", "SQL"],
+    skills: [
+      "skillsUsed.fullStack",
+      "skillsUsed.api",
+      "skillsUsed.responsiveDesign",
+      "skillsUsed.teamwork",
+    ],
   },
+
   {
     title: "camagru",
+    category: "web",
     descriptionKey: "projectDescriptions.camagru",
     img: "/assets/projects/camagru.png",
     link: "https://github.com/chrstnhu/camagru",
+    technologies: ["Web", "PHP", "SQL"],
+    skills: [
+      "skillsUsed.webDevelopment",
+      "skillsUsed.database",
+      "skillsUsed.authentication",
+    ],
   },
+
   {
     title: "matcha",
+    category: "web",
     descriptionKey: "projectDescriptions.matcha",
     img: "/assets/projects/matcha.png",
     link: "https://github.com/xchen34/matcha",
+    technologies: ["JavaScript", "Node.js", "Express", "SQL"],
+    skills: ["skillsUsed.fullStack", "skillsUsed.api", "skillsUsed.database"],
   },
+
   {
     title: "timetools",
+    category: "web",
     descriptionKey: "projectDescriptions.timetools",
     img: "/assets/projects/timetools.png",
     link: "https://github.com/chrstnhu/timetools",
+    technologies: ["React", "TypeScript"],
+    skills: [
+      "skillsUsed.uiIntegration",
+      "skillsUsed.responsiveDesign",
+      "skillsUsed.problemSolving",
+    ],
   },
 ];
